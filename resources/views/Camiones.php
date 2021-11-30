63eaa3470bf6fbb9559a33543b2fdfb6693ff295
Hector Saldivia <Hectorheck>
2021-11-30 19:03:07 -0300

    6

--- resources/views/Camiones.php
+++ resources/views/Camiones.php
@@ -8,10 +8,23 @@ function distancia($Punto1,$punto2)
     $x2 = $punto2["x"];
     $y2 = $punto2["y"];
     $distancia = sqrt(pow(($x2-$x1),2)+pow(($y2-$y1),2));
+    $distancia = bcdiv($distancia, '1', 5);
     return $distancia;
     
 
 }
+
+function ordenar($demandas,$data)
+{
+    for($i=0;$i<count($demandas);$i++)
+    {
+      $demandas[$i]["Distancia"] = distancia($data['p'][$demandas[$i]['p']],$data['c'][$demandas[$i]['c']]);
+      
+    }
+
+    return $demandas;
+}
+
 
 
 
--- resources/views/prueba.php
+++ resources/views/prueba.php
@@ -2,4 +2,16 @@
 require("Camiones.php");
 $data = distribucion('coordenadas.txt');
 $demanda = demandas('demandas.txt');
+$distancia = distancia($data['p'][1],$data['c'][1]);
+$demanda = ordenar($demanda,$data);
+
+print_r($data);
+
+echo "<br>";
+echo "<br>";
+
+echo "<br>";
+
+
+
 print_r($demanda);
