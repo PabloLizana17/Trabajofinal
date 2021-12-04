1f284c7706d9fe207aebdd2e753f19c306d03c15
pablo <pablolizana0@gmail.com>
2021-12-04 02:11:10 -0300

    archivos separados, archivo 1 pequeño problema

--- app/Http/Controllers/Txtcontrolador.php
+++ app/Http/Controllers/Txtcontrolador.php
@@ -16,25 +16,54 @@ class Txtcontrolador extends Controller
 
     public function mguardar(Request $request){
         Log::info("Obtencion de archivos");
+        
         if($request->hasFile("Archivo1"))
         {
             Log::info("Archivo 1 ingresado correctamente");
-            $a=$request->file("Archivo1");
+            $a= $request->file("Archivo1");
+            $nombre = "coordenadas.txt";
+            $ruta = public_path($nombre);
+            print_r($a);
+            var_dump($a->guessExtension());
+            if($a->guessExtension()=="txt")
+            {
+                Log::info("Su archivo es un documento de texto");
+                if(copy($a,$ruta))
+                    Log::info("Documento de texto guardado correctamente");
+                else
+                    Log::info("Error al guardar documento");
+            }
+            else
+            {
+                Log::info("Su archivo no es documento de texto, vuelva a intentarlo");
+            }
         }
         else
         {
             Log::info("Archivo 1 no ingresado");
-            print("a");
         }
         if($request->hasFile("Archivo2"))
         {
             Log::info("Archivo 2 ingresado correctamente");
             $b=$request->file("Archivo2");
+            $nombre = "demandas.txt";
+            $ruta = public_path($nombre);
+            if($b->guessExtension()=="txt")
+            {
+                Log::info("Su archivo es un documento de texto");
+                if(copy($b,$ruta))
+                    Log::info("Documento de texto guardado correctamente");
+                else
+                    Log::info("Error al guardar documento");
+            }
+            else
+            {
+                Log::info("Su archivo no es documento de texto, vuelva a intentarlo");
+            }
         }
         else
         {
             Log::info("Archivo 2 no ingresado");
-            print("b");
         }
         
     }
