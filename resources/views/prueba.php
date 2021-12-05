128bb9f2d983d3e5cc3ffda99e9292e75f4afdf1
pablo <pablolizana0@gmail.com>
2021-12-05 11:58:19 -0300

    arreglando funciones

--- resources/views/prueba.php
+++ resources/views/prueba.php
@@ -4,7 +4,7 @@
     $demanda = demandas('demandas.txt');
     $distancia = distancia($data['p'][1],$data['c'][1]);
     $demanda = ordenar($demanda,$data);
-    $ordenar = separar($demanda,$data);
+    $ordenar = separar($demanda);
     $caminos = resultados($ordenar,$data);
 
     print_r ($caminos);
