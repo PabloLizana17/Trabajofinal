a446bc9545ef44c0dcf85cfd14037cda6fa069e1
pablo <pablolizana0@gmail.com>
2021-12-05 11:42:50 -0300

    arreglando prueba.php

--- resources/views/prueba.php
+++ resources/views/prueba.php
@@ -1,13 +1,13 @@
 <?php
-require("Camiones.php");
-$data = distribucion('coordenadas.txt');
-$demanda = demandas('demandas.txt');
-$distancia = distancia($data['p'][1],$data['c'][1]);
-$demanda = ordenar($demanda,$data);
-$ordenar = separar($demanda,$data);
-$caminos = resultados($ordenar,$data);
+    require("Camiones.php");
+    $data = distribucion('coordenadas.txt');
+    $demanda = demandas('demandas.txt');
+    $distancia = distancia($data['p'][1],$data['c'][1]);
+    $demanda = ordenar($demanda,$data);
+    $ordenar = separar($demanda,$data);
+    $caminos = resultados($ordenar,$data);
 
-print_r ($caminos);
+    print_r ($caminos);
 
 ?>
 
